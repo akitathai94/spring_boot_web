62cc734f0d8cc5f60042cec2340f4b516f239a4e
Thai Truong <thai.p.truong.mil@army.mil>
2023-06-24 19:37:01 -0700

    Some implementations

---
+++ src/main/java/com/in28minutes/spring/learnspringframework/CourseController.java
@@ -0,0 +1,18 @@
+package com.in28minutes.spring.learnspringframework;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+    @RequestMapping("/courses")
+    public List<Course> retryAllCourses() {
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "in28minutes"),
+                new Course(2, "Learn Azure", "in28minutes"),
+                new Course(3, "Learn GCP", "in28minutes"));
+    }
+}
